d9a056c7748cebc965ad39691fa92b079be3da50
holger <holger@HAL3>
2015-11-02 19:03:55 +0100

    First Commit

---
+++ test-it-all/src/communication/components/parts/CentralServer.java
@@ -0,0 +1,13 @@
+package communication.components.parts;
+
+public class CentralServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
